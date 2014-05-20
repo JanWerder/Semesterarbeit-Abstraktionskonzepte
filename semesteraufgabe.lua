7dd44e4297222b3fe749fcd63794fe835b7d57be
Jan <jackflash5500@gmail.com>
2014-05-20 13:49:13 +0200

    Precondition hinzugefügt

--- semesteraufgabe.lua
+++ semesteraufgabe.lua
@@ -1,32 +1,57 @@
 local liste = {}
 function first(tbl)
+  if preTable(tbl) then
    return tbl[1]
+  else
+    return listenError
+  end
 end
 
 function rest(tbl)
-  t = tbl
-  table.remove(t, 1)
-  return t
+  if preTable(tbl) then
+    t = tbl
+    table.remove(t, 1)
+    return t
+  else
+    return listenError
+  end
 end
 
 function cons(tbl1, tbl2)
-  t = {}
-  table.insert(t, tbl1)
-  table.insert(t, tbl2)
-  return t
+  if preTable(tbl1) and preTable(tbl2) then
+    t = {}
+    table.insert(t, tbl1)
+    table.insert(t, tbl2)
+    return t
+  else
+    return listenError
+  end
 end
 
 function append(tbl1, tbl2)
-  t = {}
-  for k,v in ipairs(tbl1) do
-    table.insert(t, v)
+  if preTable(tbl1) and preTable(tbl2) then
+    t = {}
+    for k,v in ipairs(tbl1) do
+      table.insert(t, v)
+    end
+    for k,v in ipairs(tbl2) do
+      table.insert(t, v)
+    end
+    return t
+  else
+    return listenError
   end
-  for k,v in ipairs(tbl2) do
-    table.insert(t, v)
-  end
-  return t
 end
 
+function preTable (tbl)
+  if type(tbl) == type(table) then
+    return true
+  else
+    return false
+  end
+end
+
+listenError = 'Parameter ist keine Liste'
 liste.append = append
 liste.cons = cons
 liste.rest = rest
--- semestertest.lua
+++ semestertest.lua
@@ -3,21 +3,18 @@ liste = require('semesteraufgabe')
 inspect = require('inspect')
 
 firstTable = {1,2,3}
-print('First:' .. liste.first(firstTable))
+print('First: ' .. liste.first(firstTable))
 
 restTable = {'A','B','C'}
 restTable = liste.rest(restTable)
-print('Rest:')
-print(inspect(restTable))
+print('Rest: ' .. inspect(restTable))
 
 consTable1 = {{'A'},'B'}
 consTable2 = {'C','D'}
 consTable = liste.cons(consTable1, consTable2)
-print('Cons:')
-print(inspect(consTable))
+print('Cons: ' .. inspect(consTable))
 
 appendTable1 = {{'A'},'B'}
 appendTable2 = {'C','D'}
 appendTable = liste.append(appendTable1, appendTable2)
-print('Append:')
-print(inspect(appendTable))
+print('Append: ' .. inspect(appendTable))
